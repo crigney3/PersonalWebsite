75bf1dd383cd86e5f9148a348a72992a214fb56e
Corey Rigney <coreyrigney@gmail.com>
2019-09-04 15:32:18 -0400

    Added PHP email contact method

---
+++ contact.php
@@ -0,0 +1,15 @@
+<?php
+
+if($_POST["submit"]) {
+    $recipient="cbr7117@rit.edu";
+    $subject=$_POST["subject"];
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+}
